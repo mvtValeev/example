ea5f4034bacf45c53a4f4f5596a6fc9bb82aa66d
mvtValeevmail.ru <mvtValeev@mail.ru>
2021-03-01 22:35:03 +0300

    Кофейку?

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import sqlite3
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)  # Загружаем дизайн
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.run()
+
+    def run(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM coffee").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        self.tableWidget.setHorizontalHeaderLabels(self.titles)
+        # Заполнили таблицу полученными элементами
+        result.sort(key=lambda x: x[0])
+
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
